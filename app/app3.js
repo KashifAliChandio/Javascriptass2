d61687ef7ea1889d35a499e0602d49373b5e7cd6
Kashif <kashifc313@gmail.com>
2017-03-31 09:27:36 +0500

    First comment

---
+++ app/app3.js
@@ -0,0 +1,2 @@
+var arr = ["Apple " ,"banana ", "Pine Apple", "Water manon"];
+document.write(arr);
---
+++ app/app8.js
@@ -0,0 +1,5 @@
+var move = [];
+prompt(move.push);
+
+document.write(move);
+document.write(move);
